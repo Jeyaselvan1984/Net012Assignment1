0badcb585083a022df5674cae299c62ad9ef6073
Jeyaselvan <jeyaselvan.navaraj@gmail.com>
2019-12-02 14:52:19 -0500

    16 of 19 completed for assignment 2

---
+++ CareerCloud.ADODataAccessLayer/SecurityLoginsLogRepository.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Linq;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class SecurityLoginsLogRepository : IDataRepository<SecurityLoginsLogPoco>
+    {
+        private string _connStr;
+        public SecurityLoginsLogRepository()
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+        public void Add(params SecurityLoginsLogPoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                foreach (SecurityLoginsLogPoco poco in items)
+                {
+                    SqlCommand comm = new SqlCommand();
+                    comm.Connection = connection;
+                    comm.CommandText = @"INSERT INTO [dbo].[Security_Logins_Log]
+                                       ([Id]
+                                       ,[Login]
+                                       ,[Source_IP]
+                                       ,[Logon_Date]
+                                       ,[Is_Succesful])
+                                 VALUES
+                                       (Id, 
+                                        Login, 
+                                        Source_IP, 
+                                        Logon_Date, 
+                                        Is_Succesful)";
+                    comm.Parameters.AddWithValue("@Id", poco.Id);
+                    comm.Parameters.AddWithValue("@Login", poco.Login);
+                    comm.Parameters.AddWithValue("@Source_IP", poco.SourceIP);
+                    comm.Parameters.AddWithValue("@Logon_Date", poco.LogonDate);
+                    comm.Parameters.AddWithValue("@Is_Succesful", poco.IsSuccesful);
+       
+                 
+
+
+
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityLoginsLogPoco> GetAll(params Expression<Func<SecurityLoginsLogPoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT [Id]
+                                  ,[Login]
+                                  ,[Source_IP]
+                                  ,[Logon_Date]
+                                  ,[Is_Succesful]
+                              FROM [dbo].[Security_Logins_Log]";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                SecurityLoginsLogPoco[] pocos = new SecurityLoginsLogPoco[500];
+                int index = 0;
+                while (reader.Read())
+                {
+                    SecurityLoginsLogPoco poco = new SecurityLoginsLogPoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Login = Guid.Parse((string)reader["Login"]);
+                    poco.SourceIP = (string)reader["Password"];          
+                    poco.LogonDate = reader.GetDateTime(3);
+                    poco.IsSuccesful = reader.GetBoolean(4);
+      
+                   
+                    pocos[index] = poco;
+                    index++;
+                }
+                conn.Close();
+                return pocos.Where(a => a != null).ToList();
+            }
+        }
+
+        public IList<SecurityLoginsLogPoco> GetList(Expression<Func<SecurityLoginsLogPoco, bool>> where, params Expression<Func<SecurityLoginsLogPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityLoginsLogPoco GetSingle(Expression<Func<SecurityLoginsLogPoco, bool>> where, params Expression<Func<SecurityLoginsLogPoco, object>>[] navigationProperties)
+        {
+            IQueryable<SecurityLoginsLogPoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
+        }
+
+        public void Remove(params SecurityLoginsLogPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (SecurityLoginsLogPoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE Security_Logins_Log   
+                                        where ID = @id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Update(params SecurityLoginsLogPoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Security_Logins_Log]
+                                           SET [Id] = <Id, uniqueidentifier,>
+                                              ,[Login] = <Login, uniqueidentifier,>
+                                              ,[Source_IP] = <Source_IP, char(15),>
+                                              ,[Logon_Date] = <Logon_Date, datetime,>
+                                              ,[Is_Succesful] = <Is_Succesful, bit,>
+                                         WHERE [Id] = @Id";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Login", poco.Login);
+                    cmd.Parameters.AddWithValue("@Source_IP", poco.SourceIP);
+                    cmd.Parameters.AddWithValue("@Logon_Date", poco.LogonDate);
+                    cmd.Parameters.AddWithValue("@Is_Succesful", poco.IsSuccesful);
+
+                    connection.Open();
+                    int count = cmd.ExecuteNonQuery();
+                    if (count != -1)
+                    {
+                        throw new Exception();
+                    }
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/SecurityRoleRepository.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Linq;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class SecurityRoleRepository : IDataRepository<SecurityRolePoco>
+    {
+        private string _connStr;
+        public SecurityRoleRepository()
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+        public void Add(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                foreach (SecurityRolePoco poco in items)
+                {
+                    SqlCommand comm = new SqlCommand();
+                    comm.Connection = connection;
+                    comm.CommandText = @"INSERT INTO [dbo].[Security_Roles]
+                                           ([Id]
+                                           ,[Role]
+                                           ,[Is_Inactive])
+                                     VALUES
+                                           (Id,
+                                            Role,
+                                            Is_Inactive)";
+                    comm.Parameters.AddWithValue("@Id", poco.Id);
+                    comm.Parameters.AddWithValue("@Role", poco.Role);
+                    comm.Parameters.AddWithValue("@Is_Inactive", poco.IsInactive);
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityRolePoco> GetAll(params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT [Id]
+                                  ,[Role]
+                                  ,[Is_Inactive]
+                              FROM [dbo].[Security_Roles]";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                SecurityRolePoco[] pocos = new SecurityRolePoco[500];
+                int index = 0;
+                while (reader.Read())
+                {
+                    SecurityRolePoco poco = new SecurityRolePoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Role = (string)reader["Role"];
+                    poco.IsInactive = reader.GetBoolean(2);
+                    pocos[index] = poco;
+                    index++;
+                }
+                conn.Close();
+                return pocos.Where(a => a != null).ToList();
+            }
+        }
+
+        public IList<SecurityRolePoco> GetList(Expression<Func<SecurityRolePoco, bool>> where, params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityRolePoco GetSingle(Expression<Func<SecurityRolePoco, bool>> where, params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            IQueryable<SecurityRolePoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
+        }
+
+        public void Remove(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (SecurityRolePoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE Security_Roles   
+                                        where ID = @id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Update(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Security_Roles]
+                                       SET [Id] = <Id, uniqueidentifier,>
+                                          ,[Role] = <Role, varchar(50),>
+                                          ,[Is_Inactive] = <Is_Inactive, bit,>
+                                     WHERE [Id] = @Id";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Role", poco.Role);
+                    cmd.Parameters.AddWithValue("@Is_Inactive", poco.IsInactive);
+          
+
+                    connection.Open();
+                    int count = cmd.ExecuteNonQuery();
+                    if (count != -1)
+                    {
+                        throw new Exception();
+                    }
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
