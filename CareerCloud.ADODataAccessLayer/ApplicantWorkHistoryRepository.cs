1ce2d90b724fa82aaa6a4314eb3415b5e4e5cb21
Jeyaselvan <jeyaselvan.navaraj@gmail.com>
2019-12-01 20:55:39 -0500

    7 classes of Assignment2 completed

---
+++ CareerCloud.ADODataAccessLayer/ApplicantWorkHistoryRepository.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Linq;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantWorkHistoryRepository : IDataRepository<ApplicantWorkHistoryPoco>
+    {
+
+        private string _connStr;
+        public ApplicantWorkHistoryRepository()
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+        public void Add(params ApplicantWorkHistoryPoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                foreach (ApplicantWorkHistoryPoco poco in items)
+                {
+                    SqlCommand comm = new SqlCommand();
+                    comm.Connection = connection;
+                    comm.CommandText = @"INSERT INTO [dbo].[Applicant_Work_History]
+                                       ([Id]
+                                       ,[Applicant]
+                                       ,[Company_Name]
+                                       ,[Country_Code]
+                                       ,[Location]
+                                       ,[Job_Title]
+                                       ,[Job_Description]
+                                       ,[Start_Month]
+                                       ,[Start_Year]
+                                       ,[End_Month]
+                                       ,[End_Year])
+                                 VALUES
+                                       (Id, 
+                                        Applicant, 
+                                        Company_Name, 
+                                        Country_Code, 
+                                        Location, 
+                                        Job_Title,
+                                        Job_Description,
+                                        Start_Month, 
+                                        Start_Year, 
+                                        End_Month, 
+                                        End_Year)";
+                    comm.Parameters.AddWithValue("@Id", poco.Id);
+                    comm.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    comm.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    comm.Parameters.AddWithValue("@Country_Code", poco.CountryCode);
+                    comm.Parameters.AddWithValue("@Location", poco.Location);
+                    comm.Parameters.AddWithValue("@Job_Title", poco.JobTitle);
+                    comm.Parameters.AddWithValue("@Job_Description", poco.JobDescription);
+                    comm.Parameters.AddWithValue("@Start_Month", poco.StartMonth);
+                    comm.Parameters.AddWithValue("@Start_Year", poco.StartYear);
+                    comm.Parameters.AddWithValue("@End_Month", poco.EndMonth);
+                    comm.Parameters.AddWithValue("@End_Year", poco.EndYear);
+
+
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantWorkHistoryPoco> GetAll(params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @" SELECT [Id]
+                                      ,[Applicant]
+                                      ,[Company_Name]
+                                      ,[Country_Code]
+                                      ,[Location]
+                                      ,[Job_Title]
+                                      ,[Job_Description]
+                                      ,[Start_Month]
+                                      ,[Start_Year]
+                                      ,[End_Month]
+                                      ,[End_Year]
+                                      ,[Time_Stamp]
+                                  FROM [dbo].[Applicant_Work_History]";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                ApplicantWorkHistoryPoco[] pocos = new ApplicantWorkHistoryPoco[500];
+                int index = 0;
+                while (reader.Read())
+                {
+                    ApplicantWorkHistoryPoco poco = new ApplicantWorkHistoryPoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Applicant = Guid.Parse((string)reader["Applicant"]);
+                    poco.CompanyName = (string)reader["Company_Name"];
+                    poco.CountryCode = (string)reader["Country_Code"];
+                    poco.Location = (string)reader["Location"];
+                    poco.JobTitle = (string)reader["Job_Title"];
+                    poco.JobDescription = (string)reader["Job_Description"];
+                    poco.StartMonth = reader.GetInt16(7);
+                    poco.StartYear = reader.GetInt32(8);
+                    poco.EndMonth =reader.GetInt16(9);
+                    poco.EndYear = reader.GetInt32(10);
+                    poco.TimeStamp = (Byte[])reader[11];
+
+
+                    pocos[index] = poco;
+                    index++;
+                }
+                conn.Close();
+                return pocos.Where(a => a != null).ToList();
+            }
+        }
+
+        public IList<ApplicantWorkHistoryPoco> GetList(Expression<Func<ApplicantWorkHistoryPoco, bool>> where, params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantWorkHistoryPoco GetSingle(Expression<Func<ApplicantWorkHistoryPoco, bool>> where, params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
+        {
+
+            IQueryable<ApplicantWorkHistoryPoco> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
+        }
+
+        public void Remove(params ApplicantWorkHistoryPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (ApplicantWorkHistoryPoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE Applicant_Work_History
+                                        where ID = @id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Update(params ApplicantWorkHistoryPoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Applicant_Work_History]
+                                       SET [Id] = <Id, uniqueidentifier,>
+                                          ,[Applicant] = <Applicant, uniqueidentifier,>
+                                          ,[Company_Name] = <Company_Name, nvarchar(150),>
+                                          ,[Country_Code] = <Country_Code, char(10),>
+                                          ,[Location] = <Location, nvarchar(50),>
+                                          ,[Job_Title] = <Job_Title, nvarchar(50),>
+                                          ,[Job_Description] = <Job_Description, nvarchar(500),>
+                                          ,[Start_Month] = <Start_Month, smallint,>
+                                          ,[Start_Year] = <Start_Year, int,>
+                                          ,[End_Month] = <End_Month, smallint,>
+                                          ,[End_Year] = <End_Year, int,>
+                                     WHERE [Id] = @Id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    cmd.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    cmd.Parameters.AddWithValue("@Country_Code", poco.CountryCode);
+                    cmd.Parameters.AddWithValue("@Location", poco.Location);
+                    cmd.Parameters.AddWithValue("@Job_Title", poco.JobTitle);
+                    cmd.Parameters.AddWithValue("@Job_Description", poco.JobDescription);
+                    cmd.Parameters.AddWithValue("@Start_Month", poco.StartMonth);
+                    cmd.Parameters.AddWithValue("@Start_Year", poco.StartYear);
+                    cmd.Parameters.AddWithValue("@End_Month", poco.EndMonth);
+                    cmd.Parameters.AddWithValue("@End_Year", poco.EndYear);
+
+                    connection.Open();
+                    int count = cmd.ExecuteNonQuery();
+                    if (count != -1)
+                    {
+                        throw new Exception();
+                    }
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
